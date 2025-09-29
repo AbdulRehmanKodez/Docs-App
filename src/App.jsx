620892f5ba1c11a520fb33ce374ac9d4797edde4
Abdul Rehman Butt <abdulrehmanbutt.to@gmail.com>
2025-09-29 23:02:29 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' w-full h-screen bg-zinc-800  font-[meow]  '>
+      <h1 className='absolute text-[250px] leading-none tracking-tighter left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] text-zinc-950'>DOCS.</h1>
+    </div>
+  )
+}
+
+export default App 
